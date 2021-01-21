f73b22168ff9c055413acb6adfc510d66caf1841
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-01-21 14:58:18 -0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos non fuga ad similique voluptate dolore sapiente eveniet, ea inventore quas minus cum vel ipsa? Vitae quo fugit voluptates commodi excepturi perferendis eaque asperiores in voluptatem amet. Repudiandae, tempore omnis. Tempore neque delectus, aperiam repellendus illum dolor ea ullam nemo debitis?</p>
+    </section>
+  )
+}
+
+export default About
